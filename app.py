0c0449c5e3bb7c5df6ce1490b24cc839ed365fb0
godot63 <lcalmbach@gmail.com>
2021-09-28 07:40:53 +0200

    Add lottie

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+from altair.vegalite.v4.schema.core import Align
 import streamlit as st 
+from streamlit_lottie import st_lottie
 import first_names
 import surnames
+import requests
 
 __version__ = '0.0.7'
 __author__ = 'Lukas Calmbach'
@@ -15,10 +18,28 @@ APP_INFO = f"""<div style="background-color:powderblue; padding: 10px;border-rad
     <a href="{GIT_REPO}">git-repo</a>
     """
 
+@st.experimental_memo()
+def get_lottie():
+    ok=True
+    r=''
+    try:
+        r = requests.get('https://assets5.lottiefiles.com/packages/lf20_co3tq7q4.json').json()
+    except:
+        ok = False
+    return r,ok
+    
+    
+
 def main():
     st.set_page_config(
         page_title=my_name,
         layout="wide")
+    
+    lottie_search_names,ok = get_lottie()
+    if ok:
+        st_lottie(lottie_search_names,height=80, loop=False)
+    else:
+        st.write(123)
     st.sidebar.markdown(f"## 👫🏽 {my_name}")
     menu_action = st.sidebar.selectbox('Menu',['Vornamen','Nachnamen'])
     if menu_action == 'Vornamen':
